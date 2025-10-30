f956b87ac9463724c055c26081cd41c10a0931ab
Rohan Kukreja <rhnkukreja@gmail.com>
2025-10-30 13:56:52 +0530

    Added openai in requirements.txt

--- main.py
+++ main.py
@@ -313,6 +313,6 @@ async def root():
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run("main_new:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
 
 
